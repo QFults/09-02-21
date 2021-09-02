f51d8280a4a6b74b7c4a19a4af72f313cd7552d9
Quinton Fults <quinton.fults.work@gmail.com>
2021-09-02 13:10:47 -0700

    code

--- index.js
+++ index.js
@@ -34,20 +34,47 @@
 
 // console.log(dog1, dog2)
 
-const dog = (name, age, breed) => ({
-  name,
-  age,
-  breed,
-  bark() {
-    console.log(`Woof! My name is ${this.name}`)
-  }
-})
+// const dog = (name, age, breed) => ({
+//   name,
+//   age,
+//   breed,
+//   bark() {
+//     console.log(`Woof! My name is ${this.name}`)
+//   }
+// })
 
-const chihuahua = (name, age, breed, aggression) => ({ ...dog(name, age, breed), aggression })
+// const chihuahua = (name, age, breed, aggression) => ({ ...dog(name, age, breed), aggression })
 
-const dog1 = chihuahua('Beef', 2, 'chihuahua', '15%')
-
-console.log(dog1)
-dog1.bark()
+// const dog1 = chihuahua('Beef', 2, 'chihuahua', '15%')
 
 // console.log(dog1)
+// dog1.bark()
+
+// console.log(dog1)
+
+class Dog {
+  constructor (name, age, breed) {
+    this.name = name
+    this.age = age
+    this.breed = breed
+    // this.bark = function () {
+    //   console.log(`Woof! My name is ${this.name}`)
+    // }
+  }
+
+  bark () {
+    console.log(`Woof! My name is ${this.name}`)
+  }
+}
+
+class Chihuahua extends Dog {
+  constructor (name, age, breed, aggression) {
+    super(name, age, breed)
+    this.aggression = aggression
+  }
+}
+
+const dog1 = new Chihuahua('Beef', 2, 'chihuahua', '15%')
+
+dog1.bark()
+console.log(dog1)
