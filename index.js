a43e2eea9b78afe67fa4a64e336f64e00d9b7998
Quinton Fults <quinton.fults.work@gmail.com>
2021-09-02 10:52:49 -0700

    code

---
+++ index.js
@@ -0,0 +1,35 @@
+// let dog1 = {
+//   name: 'Beef',
+//   age: 2,
+//   breed: 'Chihuahua'
+// }
+
+// let dog2 = {
+//   name: 'Bear',
+//   age: 5,
+//   breed: 'Cockapoo'
+// }
+
+// function Dog (name, age, breed) {
+//   this.name = name
+//   this.age = age
+//   this.breed = breed
+//   this.bark = function () {
+//     console.log(`Woof! My name is ${this.name}`)
+//   }
+// }
+
+// function Chihuahua (name, age, breed, aggression) {
+//   Dog.call(this, name, age, breed)
+//   this.aggression = aggression
+// }
+
+// const dog1 = new Chihuahua('Beef', 2, 'chihuahua', '15%')
+// dog1.aggression = '15%'
+
+// const dog2 = new Dog('Bear', 5, 'cockapoo')
+
+// dog1.bark()
+// dog2.bark()
+
+// console.log(dog1, dog2)
